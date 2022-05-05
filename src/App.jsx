fb78d3f585e4cadf8813733b0cfd286c25815e1d
abhaskar007 <abhaskar.bhaskar@gmail.com>
2022-05-05 19:25:56 +0530

    Navbar css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import react from "react";
 import "./App.css";
+import Home from "./components/HomePage/Home";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home/>
     </div>
   );
 }
